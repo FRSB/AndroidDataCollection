39767530acac4cb07238cb383b83adea02c7de2a
FRosner <frank.rosner@student.uni-halle.de>
2013-02-20 16:13:59 +0100

    share android project

---
+++ AndroidDataCollection/gen/de/unihalle/ebusiness/androiddatacollection/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package de.unihalle.ebusiness.androiddatacollection;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
