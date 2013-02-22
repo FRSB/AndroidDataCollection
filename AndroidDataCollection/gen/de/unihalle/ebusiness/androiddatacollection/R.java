777cf97a0e996b9b8e93b11f669da46d6ecad55c
sbarsbar1 <sebastian.baer@student.uni-halle.de>
2013-02-22 12:49:10 +0100

    - first test implementing gps sensor data retriever

--- AndroidDataCollection/gen/de/unihalle/ebusiness/androiddatacollection/R.java
+++ AndroidDataCollection/gen/de/unihalle/ebusiness/androiddatacollection/R.java
@@ -24,9 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080002;
-        public static final int progressBar1=0x7f080001;
+        public static final int action_settings=0x7f080006;
+        public static final int beginEndButton=0x7f080001;
+        public static final int latitude=0x7f080003;
+        public static final int longitude=0x7f080005;
         public static final int textView1=0x7f080000;
+        public static final int text_latitude=0x7f080002;
+        public static final int text_longitude=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -37,6 +41,8 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int button_begin=0x7f050003;
+        public static final int button_end=0x7f050004;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
--- AndroidDataCollection/src/de/unihalle/ebusiness/androiddatacollection/MainActivity.java
+++ AndroidDataCollection/src/de/unihalle/ebusiness/androiddatacollection/MainActivity.java
@@ -1,23 +1,89 @@
 package de.unihalle.ebusiness.androiddatacollection;
 
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	private Button beginEndButton;
+	private LocationManager locationManager;
+	private LocationListener locationListener;
+	private Location location;
+	public TextView latitude;
+	public TextView longitude;
+	public TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.activity_main);        
+        
+        try {
+			beginEndButton = ((Button) findViewById(R.id.beginEndButton));
+			latitude = ((TextView) findViewById(R.id.latitude));
+			longitude = ((TextView) findViewById(R.id.longitude));
+			textView = ((TextView) findViewById(R.id.textView1));
+			
+			locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+			location = locationManager.getLastKnownLocation("gps");
+			
+			locationListener = new LocationListener() {
+				
+				@Override
+				public void onStatusChanged(String provider, int status, Bundle extras) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void onProviderEnabled(String provider) {
+					// TODO Auto-generated method stub
+					
+				}
+				
+				@Override
+				public void onProviderDisabled(String provider) {
+					textView.setText("GPS is deactivated");
+					
+				}
+				
+				@Override
+				public void onLocationChanged(Location location) {
+					
+					latitude.setText(Double.toString(location.getLatitude()));
+					longitude.setText(Double.toString(location.getLongitude()));
+										
+				}
+			};
+			
+			Button.OnClickListener listener = new Button.OnClickListener() {
 
+				@Override
+				public void onClick(View arg0) {				
+										
+					if (beginEndButton.getText() == getString(R.string.button_begin)) {
+						beginEndButton.setText(getString(R.string.button_end));
+					} else {
+						beginEndButton.setText(getString(R.string.button_begin));
+					}
+				}
+				
+			};
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);        
-        return true;
+			((Button) findViewById(R.id.beginEndButton)).setOnClickListener(listener);
+			locationManager.requestLocationUpdates("gps", 30000L, 10.0f, locationListener);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
     }
     
 }
