5dc36b83be0aca43c2d113ff73a163eddf0a5904
sbarsbar1 <sebastian.baer@student.uni-halle.de>
2013-02-25 18:33:13 +0100

    - more sensors implemented and shown

--- AndroidDataCollection/gen/de/unihalle/ebusiness/androiddatacollection/R.java
+++ AndroidDataCollection/gen/de/unihalle/ebusiness/androiddatacollection/R.java
@@ -24,13 +24,34 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int beginEndButton=0x7f080001;
-        public static final int latitude=0x7f080003;
-        public static final int longitude=0x7f080005;
-        public static final int textView1=0x7f080000;
-        public static final int text_latitude=0x7f080002;
-        public static final int text_longitude=0x7f080004;
+        public static final int accelerometer_x=0x7f080004;
+        public static final int accelerometer_y=0x7f080006;
+        public static final int accelerometer_z=0x7f080008;
+        public static final int action_settings=0x7f08001b;
+        public static final int beginEndButton=0x7f080002;
+        public static final int cellId=0x7f08000a;
+        public static final int cellLac=0x7f08000c;
+        public static final int cellNeighbors=0x7f08000e;
+        public static final int gps_accuracy=0x7f080010;
+        public static final int gps_altitude=0x7f080012;
+        public static final int gps_bearing=0x7f080018;
+        public static final int gps_latitude=0x7f080014;
+        public static final int gps_longiitude=0x7f080016;
+        public static final int gps_speed=0x7f08001a;
+        public static final int scrollView1=0x7f080000;
+        public static final int textView1=0x7f080001;
+        public static final int text_accelerometer_x=0x7f080003;
+        public static final int text_accelerometer_y=0x7f080005;
+        public static final int text_accelerometer_z=0x7f080007;
+        public static final int text_cellLac=0x7f08000b;
+        public static final int text_cellNeighbors=0x7f08000d;
+        public static final int text_cellid=0x7f080009;
+        public static final int text_gps_accuracy=0x7f08000f;
+        public static final int text_gps_altitude=0x7f080011;
+        public static final int text_gps_bearing=0x7f080017;
+        public static final int text_gps_latitude=0x7f080013;
+        public static final int text_gps_longitude=0x7f080015;
+        public static final int text_gps_speed=0x7f080019;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- AndroidDataCollection/src/de/unihalle/ebusiness/androiddatacollection/MainActivity.java
+++ AndroidDataCollection/src/de/unihalle/ebusiness/androiddatacollection/MainActivity.java
@@ -1,11 +1,20 @@
 package de.unihalle.ebusiness.androiddatacollection;
 
+import java.util.List;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Context;
+import android.telephony.NeighboringCellInfo;
+import android.telephony.TelephonyManager;
+import android.telephony.gsm.GsmCellLocation;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,26 +22,201 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 	
 	private Button beginEndButton;
+	
+	private TelephonyManager telephonyManager;
+	
 	private LocationManager locationManager;
 	private LocationListener locationListener;
-	private Location location;
-	public TextView latitude;
-	public TextView longitude;
-	public TextView textView;
+	
+	private SensorEventListener sensorEventListener;
+	private SensorManager sensorManager;
+	private Sensor accelerometerSensor;
+	private Sensor gyroscopeSensor;
+	private Sensor magneticFieldSensor;
+	private Sensor gravitySensor;
+	private Sensor lightSensor;
+	private Sensor proximitySensor;
+	private Sensor linearAccelerationSensor;
+	
+	private TextView tvAccelerometer_x;
+	private TextView tvAccelerometer_y;
+	private TextView tvAccelerometer_z;
+	
+	private TextView tvCellId;
+	private TextView tvCellLac;
+	private TextView tvCellNeighbors;
 
+	private TextView tvGpsAccuracy;
+	private TextView tvGpsAltitude;
+	private TextView tvGpsLatitude;
+	private TextView tvGpsLongitude;
+	private TextView tvGpsBearing;
+	private TextView tvGpsSpeed;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);        
         
-        try {
-			beginEndButton = ((Button) findViewById(R.id.beginEndButton));
-			latitude = ((TextView) findViewById(R.id.latitude));
-			longitude = ((TextView) findViewById(R.id.longitude));
-			textView = ((TextView) findViewById(R.id.textView1));
+        try {     	
+
 			
-			locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-			location = locationManager.getLastKnownLocation("gps");
+			beginEndButton = ((Button) findViewById(R.id.beginEndButton));
+				
+			Button.OnClickListener buttonListener = new Button.OnClickListener() {
+
+				@Override
+				public void onClick(View arg0) {				
+										
+					if (beginEndButton.getText() == getString(R.string.button_begin)) {
+						beginEndButton.setText(getString(R.string.button_end));
+			        	setUpSensors();
+						setUpSensorListener();
+						getCellLocation();
+						getGpsLocation();
+					} else {
+						beginEndButton.setText(getString(R.string.button_begin));
+						sensorManager.unregisterListener(sensorEventListener);
+						locationManager.removeUpdates(locationListener);
+					}
+				}
+				
+			};
+
+			((Button) findViewById(R.id.beginEndButton)).setOnClickListener(buttonListener);
+			
+
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }    
+       
+    public void setUpSensors() {
+    	
+    	try {
+    		sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    		
+			if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+				accelerometerSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+			}
+			
+			if (sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD) != null) {
+				magneticFieldSensor = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+			}
+			
+			if (sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY) != null) {
+				gravitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
+			}
+			
+			if (sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) != null) {
+				gyroscopeSensor = sensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+			}
+			
+			if (sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT) != null) {
+				lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+			}
+			
+			if (sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION) != null) {
+				linearAccelerationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+			}
+			
+			if (sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY) != null) {
+				proximitySensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+    }
+    
+    public void setUpSensorListener() {
+    	try {
+    		
+        	tvAccelerometer_x = (TextView) findViewById(R.id.accelerometer_x);
+        	tvAccelerometer_y = (TextView) findViewById(R.id.accelerometer_y);
+        	tvAccelerometer_z = (TextView) findViewById(R.id.accelerometer_z);
+        	
+			sensorEventListener = new SensorEventListener() {
+				
+				@Override
+				public void onSensorChanged(SensorEvent event) {
+					// get all sensor data
+					if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+						tvAccelerometer_x.setText(Float.toString(event.values[0]));
+						tvAccelerometer_y.setText(Float.toString(event.values[1]));
+						tvAccelerometer_z.setText(Float.toString(event.values[2]));
+					}
+				}
+				
+				@Override
+				public void onAccuracyChanged(Sensor sensor, int accuracy) {
+									
+				}
+			};
+			
+			sensorManager.registerListener(sensorEventListener, accelerometerSensor, SensorManager.SENSOR_DELAY_NORMAL);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    
+    public void getCellLocation() {
+    	try {
+    		   		
+        	tvCellId = (TextView) findViewById(R.id.cellId);
+        	tvCellLac = (TextView) findViewById(R.id.cellLac);
+        	tvCellNeighbors = (TextView) findViewById(R.id.cellNeighbors);
+        	
+        	        	
+			telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+			
+			GsmCellLocation cellLocation = (GsmCellLocation) telephonyManager.getCellLocation();
+			
+			tvCellId.setText(Integer.toString(cellLocation.getCid()));
+			tvCellLac.setText(Integer.toString(cellLocation.getLac()));
+			
+			List<NeighboringCellInfo> neighboringCellInfo = telephonyManager.getNeighboringCellInfo();
+			
+			String neighboringCellString = "CellID LAC RSSI | ";
+			for (NeighboringCellInfo cell : neighboringCellInfo) {
+				neighboringCellString = neighboringCellString + cell.getCid() + ", " + cell.getLac() + ", " + cell.getRssi() + " | ";
+			}
+			
+			tvCellNeighbors.setText(neighboringCellString);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        
+    }
+    
+    public void getGpsLocation() {
+    	try {
+			locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);			
+						   	
+			Location location = locationManager.getLastKnownLocation("gps");
+			
+			tvGpsAccuracy = (TextView) findViewById(R.id.gps_accuracy);
+			tvGpsAltitude = (TextView) findViewById(R.id.gps_altitude);
+			tvGpsLatitude = (TextView) findViewById(R.id.gps_latitude);
+			tvGpsLongitude = (TextView) findViewById(R.id.gps_longiitude);
+			tvGpsBearing = (TextView) findViewById(R.id.gps_bearing);
+			tvGpsSpeed = (TextView) findViewById(R.id.gps_speed);
+			
+			if (location != null) {
+				tvGpsAccuracy.setText(Double.toString(location.getAccuracy()));
+				tvGpsAltitude.setText(Double.toString(location.getAltitude()));
+				tvGpsLatitude.setText(Double.toString(location.getLatitude()));
+				tvGpsLongitude.setText(Double.toString(location.getLongitude()));
+				tvGpsBearing.setText(Double.toString(location.getBearing()));
+				tvGpsSpeed.setText(Double.toString(location.getSpeed()));
+			}
 			
 			locationListener = new LocationListener() {
 				
@@ -50,35 +234,22 @@ public class MainActivity extends Activity {
 				
 				@Override
 				public void onProviderDisabled(String provider) {
-					textView.setText("GPS is deactivated");
+					// TODO Auto-generated method stub
 					
 				}
 				
 				@Override
 				public void onLocationChanged(Location location) {
-					
-					latitude.setText(Double.toString(location.getLatitude()));
-					longitude.setText(Double.toString(location.getLongitude()));
-										
+			    	tvGpsAccuracy.setText(Double.toString(location.getAccuracy()));
+			    	tvGpsAltitude.setText(Double.toString(location.getAltitude()));
+			    	tvGpsLatitude.setText(Double.toString(location.getLatitude()));
+			    	tvGpsLongitude.setText(Double.toString(location.getLongitude()));
+			    	tvGpsBearing.setText(Double.toString(location.getBearing()));
+			    	tvGpsSpeed.setText(Double.toString(location.getSpeed()));				
 				}
 			};
 			
-			Button.OnClickListener listener = new Button.OnClickListener() {
-
-				@Override
-				public void onClick(View arg0) {				
-										
-					if (beginEndButton.getText() == getString(R.string.button_begin)) {
-						beginEndButton.setText(getString(R.string.button_end));
-					} else {
-						beginEndButton.setText(getString(R.string.button_begin));
-					}
-				}
-				
-			};
-
-			((Button) findViewById(R.id.beginEndButton)).setOnClickListener(listener);
-			locationManager.requestLocationUpdates("gps", 30000L, 10.0f, locationListener);
+			locationManager.requestLocationUpdates("gps", 30000L, 1, locationListener);
 			
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
