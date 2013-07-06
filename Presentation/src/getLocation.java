91856f35f7abc8181c4cbd64d33e1a92d5f2a922
sbarsbar1 <sebastian.baer@student.uni-halle.de>
2013-07-06 17:16:45 +0200

    changed code

--- Presentation/src/getLocation.java
+++ Presentation/src/getLocation.java
@@ -1,22 +1,15 @@
-public void getLocation() {
-   	try {
-		locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);			
-					   				
-		locationListener = new LocationListener() {					
+locationListener = new LocationListener() {
+		...
+
+		@Override
+		public void onLocationChanged(Location location) {
+			collectedDataMap.put("gpsaccuracy", Double.toString(location.getAccuracy()));
+			collectedDataMap.put("gpslatitude", Double.toString(location.getLatitude()));
+			collectedDataMap.put("gpslongitude", Double.toString(location.getLongitude()));
 			...
-			
-			@Override
-			public void onLocationChanged(Location location) {
-				collectedDataMap.put("gpsaccuracy", Double.toString(location.getAccuracy()));
-				collectedDataMap.put("gpslatitude", Double.toString(location.getLatitude()));
-				collectedDataMap.put("gpslongitude", Double.toString(location.getLongitude()));
-				locationManager.removeUpdates(locationListener);
-			}
-		};
-		
-		if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-			locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER,
-				10000, 100, locationListener);
 		}
-...
-}
+};
+
+	
+	
+	
